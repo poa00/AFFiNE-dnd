e33caf8335178a44905a9be6f9e725d316cba2ba
DarkSky <darksky2048@gmail.com>
2024-05-15 22:36:35 +0800

    chore: remove outdated types

--- packages/backend/server/src/core/auth/controller.ts
+++ packages/backend/server/src/core/auth/controller.ts
@@ -24,8 +24,6 @@ import { TokenService, TokenType } from './token';
 class SignInCredential {
   email!: string;
   password?: string;
-  token?: string;
-  challenge?: string;
 }
 
 class MagicLinkCredential {
