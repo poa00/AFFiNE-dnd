erfuuan/AFFiNE

fix: module resolve

packages/backend/server/src/core/auth/index.ts
@@ -12,7 +12,7 @@ import { TokenService } from './token';
 @Module({
   imports: [FeatureModule, UsersModule],
   providers: [AuthService, AuthResolver, SessionService, TokenService],
-  exports: [AuthService, SessionService],
+  exports: [AuthService, SessionService, TokenService],
   controllers: [AuthController, AuthSessionController],
 })
 export class AuthModule {}

poa00/AFFiNE-dnd

packages/backend/server/src/core/auth/index.ts
@@ -14,7 +14,7 @@ import { TokenService, TokenType } from './token';
 @Module({
   imports: [FeatureModule, UserModule, QuotaModule],
   providers: [AuthService, AuthResolver, TokenService, AuthGuard],
-  exports: [AuthService, AuthGuard],
+  exports: [AuthService, AuthGuard, TokenService],
   controllers: [AuthController],
 })
 export class AuthModule {}